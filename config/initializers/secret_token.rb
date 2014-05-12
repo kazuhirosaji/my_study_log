1ba530075154b491ccfc54cf322e4f1bb7225e1b
kazuhirosaji <kazuhirosaji@gmail.com>
2014-05-12 08:28:08 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyStudyLog::Application.config.secret_key_base = '9f5e7c435d4988870fbea972d3f324a5898625a740b06379460b3ca1c392e5bb12e2df379295820aa63c6f19268a9eeaff1f4a18377cb9455d6b0706c2874188'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MyStudyLog::Application.config.session_store :cookie_store, key: '_my_study_log_session'
