fbdc0bc342706764d26bf5bc0fd0ceae26d6b5d7
kazuhirosaji <kazuhirosaji@gmail.com>
2014-05-13 08:44:38 -0700

    add first test (failed)

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  describe "Home page" do
+    before { visit root_path }
+    it { should have_content('My Studylog')}
+    it { should have_title('My Studylog')}
+  end
+end
