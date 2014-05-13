045572809858ac013e9e01021784d2d0b12229c0
kazuhirosaji <kazuhirosaji@gmail.com>
2014-05-13 09:05:42 -0700

    Home page prototype

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 MyStudyLog::Application.routes.draw do
+  root 'static_pages#home'
+  get "static_pages/help"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  subject { page }
+
   describe "Home page" do
     before { visit root_path }
     it { should have_content('My Studylog')}
