d8ae14ed73be6e23baffc82ffd9d5431049efd6e
kazuhirosaji <kazuhirosaji@gmail.com>
2014-06-12 07:46:33 -0700

    impl subject routing and controller

---
+++ app/controllers/subjects_controller.rb
@@ -0,0 +1,30 @@
+class SubjectsController < ApplicationController
+  before_action :signed_in_user
+  before_action :correct_user, only: :destroy
+
+  def create
+    @subject = current_user.subjects.build(subject_params)
+    if @subject.save
+      flash[:success] = "New Subject created!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
+  end
+
+  def destroy
+    @subject.destroy
+    redirect_to root_url
+  end
+
+  private
+
+    def subject_params
+      params.require(:micropost).permit(:name)
+    end
+
+    def correct_user
+      @subject = current_user.subjects.find_by(id: params[:id])
+      redirect_to root_url if @subject.nil?
+    end
+end
