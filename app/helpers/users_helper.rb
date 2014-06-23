b7705dd0e55f2c8890a7e181ab83b85ec49ebb12
kazuhirosaji <kazuhirosaji@gmail.com>
2014-06-23 07:29:13 -0700

    impl have_subject? function

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,4 +5,8 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
+  
+  def have_subject?(user)
+    user.subjects.count > 0
+  end
 end
