8d76769ec365cd000f756510b41ff6cbebaac296
kazuhirosaji <kazuhirosaji@gmail.com>
2014-06-27 21:38:05 -0700

    create Mark table by [bundle exec rake dg:migrate] [bundle exec rake test:prepare] command

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140602123434) do
+ActiveRecord::Schema.define(version: 20140628043335) do
+
+  create_table "marks", force: true do |t|
+    t.string   "date"
+    t.integer  "subject_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "subjects", force: true do |t|
     t.string   "name"
