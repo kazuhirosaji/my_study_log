e7cb9410c531553104d6762a26149e48446f2da6
kazuhirosaji <kazuhirosaji@gmail.com>
2014-08-01 23:03:54 -0700

    hidden debug_text field

--- spec/requests/mark_pages_spec.rb
+++ spec/requests/mark_pages_spec.rb
@@ -107,10 +107,11 @@ describe "Mark pages" do
         mark.save
       end
       sign_in user
+      click_button "Save Events"
     end
-    it { should have_content(@subject.name) }
-    it { should have_content(@mark[0].date) }
-    it { should have_content(@mark[1].date) }
+    it "should have saved Marks" do
+      Mark.count.should == 2
+    end
   end
 end
 
